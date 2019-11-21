030acd4575dea1be2f73ddfbaf2789d27548a668
Fatoni <mohfatoni@yahoo.com>
2019-11-21 22:13:26 +0700

    Add basic component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "semantic-ui-css/semantic.min.css";
+import {
+  Button,
+  Grid,
+  Container,
+  Divider,
+  Flag,
+  Header,
+  Image,
+  Icon,
+  Input,
+  Label
+} from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Container textAlign="left">Kiri</Container>
+        <Container textAlign="right">kanan</Container>
+        <Container textAlign="center">tengah</Container>
+        <Container textAlign="justified">kiri kanan</Container>
+        <Divider horizontal>BATAS</Divider>
+        <Grid container columns={3}>
+          <Grid.Column>
+            <h1>Selamat Datang React Semantic UI</h1>
+            <Button primary>Button Pertama</Button>
+            <Button secondary>Button Kedua</Button>
+            <Button basic color="red">
+              Button Kedua
+            </Button>
+          </Grid.Column>
+          <Grid.Column>
+            <Flag name="id" />
+            <Flag name="indonesia" />
+          </Grid.Column>
+          <Grid.Column>
+            <Header as="h3" color="red">
+              <Image src="https://react.semantic-ui.com/images/avatar/large/patrick.png" />
+              Mohammad Fatoni
+            </Header>
+          </Grid.Column>
+        </Grid>
+        <Container textAlign="center">
+          <Icon name="spinner" size="big" loading />
+        </Container>
+        <Image
+          src="https://www.google.co.id/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png"
+          size="tiny"
+          href="https://google.com"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        />
+        <Input type="text" placeholder="search" icon={<Icon name="search" />} />
+        <Label pointing="left">Mesin Pencarian Google</Label>
+      </div>
+    );
+  }
 }
 
 export default App;
